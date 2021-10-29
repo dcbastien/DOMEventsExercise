a2c0580e1fb2a6c655806f401cc5ef069b0a492d
dcbastien <ddcbastien@gmail.com>
2021-10-29 12:14:42 -0500

    Created boilerplate, inital html and intial css  and added questions for exercise to js file

---
+++ app.js
@@ -0,0 +1,13 @@
+// DOM Events Exercise 10/29/2021
+
+// 1a. Select the 1st button element and set it to a const variable called button1
+
+// 1b. Using the ONCLICK event property, have an alert that says "Cool! You can hear/understand a hummingbird and ants" pop up when you click on the button
+
+// 2a. Select the h3 and set it to a const variable called h3
+
+// 2b. Using addEventListener, have an alert that says "OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one" pop up when you hover over the h3
+
+// 3a. Select the form element/tag and set it to a const variable called form
+
+// 3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.
